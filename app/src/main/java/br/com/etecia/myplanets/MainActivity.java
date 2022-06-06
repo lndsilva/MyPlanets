99c769cdc251ba2e22947bace214c9f42abbe83c
lndsilva <lndsilva@hotmail.com>
2022-06-06 19:29:52 -0300

    lista planets

--- app/src/main/java/br/com/etecia/myplanets/MainActivity.java
+++ app/src/main/java/br/com/etecia/myplanets/MainActivity.java
@@ -3,12 +3,48 @@ package br.com.etecia.myplanets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    //Declarar a lista
+    ListView listaPlanetas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaPlanetas = findViewById(R.id.idListaPlanetas);
+
+        listaPlanetas.setAdapter();
+
     }
+
+    //criando a classe interna - inner class
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
+
 }
